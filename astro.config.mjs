40122ff630dd161f4ce20de89f6a66212d874bd9
Pandicap <pandeli.kapurani00@gmail.com>
2026-02-04 20:53:00 +0100

    Prepare for deployment with SEO optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://duefratelli-dresden.de',
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: 'de',
+      locales: {
+        en: 'en',
+        de: 'de'
+      }
+    }
+  })],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de'],
